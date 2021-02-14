a2290e9440b8fe844fec30749a583a8b3a4abc12
Abenezer <abentad4@gmail.com>
2021-02-14 21:21:20 +0300

    first commit homePage thisweek info ui done

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+TextStyle kboldTitleStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 24.0,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle kboldTitleWhiteStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 24.0,
+  fontWeight: FontWeight.w600,
+);
+
+double kdefaultPadding = 10.0;
